8128ed98f3973176c8b8db39bbe19cbcc773485d
Cristel Chandre <cchandre@gmail.com>
2021-03-28 13:58:51 +0200

    Update qp_fk.py
    Creation of a qpFK class
    Removed global variables
    still to be debuggeg...

--- qp_fk.py
+++ qp_fk.py
@@ -2,78 +2,99 @@ import numpy as xp
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import multiprocessing
-from functools import partial
 from scipy.io import savemat
 import time
 from datetime import date
 import warnings
 warnings.filterwarnings("ignore")
 
-N = 2 ** 10
-alpha = [1.246979603717467, 2.801937735804838]
-potential = 'pot1'
-n_eps = 1024
-eps_vecs = xp.linspace([0.007, 0.0025], [0.014,  0.0038], n_eps)
-TolMax = 1e5
-TolMin = 1e-5
-threshold = 1e-7
+def main():
+	dict_params = {
+		'N': 2 ** 10,
+		'alpha': [1.246979603717467, 2.801937735804838],
+		'potential': 'pot1',
+		'n_eps': 1024,
+		'eps_lim': [[0.007, 0.0025], [0.014,  0.0038]],
+		'TolMax': 1e5,
+		'TolMin': 1e-5,
+		'threshold': 1e-7,
+		'Precision': 64
+		}
+	alpha = dict_params['alpha']
+	dv = {
+		'pot1': lambda phi, eps: eps[0] * alpha[0] * xp.sin(phi[0]) + eps[1] * alpha[1] * xp.sin(phi[1]),
+		'pot2': lambda phi, eps: eps[0] * (alpha[0] + alpha[1]) * xp.sin(2.0 * phi[0]+ 2.0 * phi[1])\
+		 + eps[1] * (alpha[0] * xp.sin(phi[0]) + alpha[1] * xp.sin(phi[1]))
+		}.get(dict_params['potential'], 'pot1')
+	case = qpFK(dv, dict_params)
+	timestr = time.strftime("%Y%m%d_%H%M")
+	num_cores = multiprocessing.cpu_count()
+	pool = multiprocessing.Pool(num_cores)
+	data = []
+	eps_vecs = xp.linspace(eps_lim, n_eps)
+	for eps2 in tqdm(eps_vecs[1]):
+		converge_point_ = lambda eps1: converge_point(eps1, eps2=eps2, case=case)
+		for result in pool.imap(converge_point_, iterable=eps_vecs[0]):
+			data.append(result)
+		save_data('qpFK_converge_region', data, timestr, case)
+	data = xp.array(data).reshape((case.n_eps, case.n_eps, 2))
+	self.save_data('qpFK_converge_region', data, timestr, case)
+	plt.pcolor(data[:, :, 0])
+	plt.show()
 
-dim = len(alpha)
-zero_ = dim * (0,)
-ind_nu = dim * (xp.fft.fftfreq(N, d=1/N),)
-nu = xp.meshgrid(*ind_nu, indexing='ij')
-alpha_nu = 2.0 * xp.pi * xp.einsum('i,i...->...', alpha, nu)
-exp_alpha_nu = xp.exp(1j * alpha_nu)
-lk_alpha_nu = 2.0 * (xp.cos(alpha_nu) - 1.0)
-ilk_alpha_nu = xp.divide(1.0, lk_alpha_nu, where=lk_alpha_nu!=0)
-sml_div = exp_alpha_nu - 1.0
-sml_div = xp.divide(1.0, sml_div, where=sml_div!=0)
-ind_phi = dim * (2.0 * xp.pi * xp.linspace(0.0, 1.0, N, endpoint=False),)
-phi = xp.meshgrid(*ind_phi, indexing='ij')
-threshold *= N**dim
+class qpFK:
+	def __init__(self, dv, dict_params):
+		for key in dict_param:
+            setattr(self, key, dict_params[key])
+		self.DictParams = dict_params
+		self.Precision = {32: xp.float32, 64: xp.float64, 128: xp.float128}.get(self.Precision, xp.float64)()
+		self.dv = dv
+		dim = len(self.alpha)
+		self.alpha = xp.asarray(self.alpha, dtype=self.Precision)
+		self.zero_ = dim * (0,)
+		ind_nu = dim * (xp.fft.fftfreq(self.N, d=1/self.N),)
+		nu = xp.meshgrid(*ind_nu, indexing='ij')
+		self.alpha_nu = 2.0 * xp.pi * xp.einsum('i,i...->...', self.alpha, nu)
+		self.exp_alpha_nu = xp.exp(1j * self.alpha_nu)
+		self.lk_alpha_nu = 2.0 * (xp.cos(self.alpha_nu) - 1.0)
+		self.sml_div = self.exp_alpha_nu - 1.0
+		self.sml_div = xp.divide(1.0, self.sml_div, where=self.sml_div!=0)
+		ind_phi = dim * (2.0 * xp.pi * xp.linspace(0.0, 1.0, self.N, endpoint=False),)
+		self.phi = xp.meshgrid(*ind_phi, indexing='ij')
+		self.threshold *= self.N**dim
+		ilk_alpha_nu = xp.divide(1.0, self.lk_alpha_nu, where=self.lk_alpha_nu!=0)
+		self.initial_h = lambda eps: - xp.fft.ifftn(xp.fft.fftn(self.dv(self.phi, eps)) * ilk_alpha_nu)
 
-def dv(phi_, eps):
-	if potential == 'pot1':
-		return eps[0] * alpha[0] * xp.sin(phi_[0]) + eps[1] * alpha[1] * xp.sin(phi_[1])
-	elif potential == 'pot2':
-		return eps[0] * (alpha[0] + alpha[1]) * xp.sin(2.0 * phi_[0]+ 2.0 * phi_[1]) + eps[1] * (alpha[0] * xp.sin(phi_[0]) + alpha[1] * xp.sin(phi_[1]))
+	def __repr__(self):
+        return '{self.__class__.name__}({self.dv, self.DictParams})'.format(self=self)
 
-def refine_h(h, lam, eps):
-	fft_h = xp.fft.fftn(h)
-	fft_h[xp.abs(fft_h) <= threshold] = 0.0
-	h_thresh = xp.fft.ifftn(fft_h)
-	arg_v = phi + 2.0 * xp.pi * xp.tensordot(xp.asarray(alpha), h_thresh, axes=0)
-	l = 1.0 + xp.fft.ifftn(1j * alpha_nu * fft_h)
-	epsilon = xp.fft.ifftn(lk_alpha_nu * fft_h) + lam + dv(arg_v, eps)
-	fft_l_eps = xp.fft.fftn(l * epsilon)
-	fft_l = xp.fft.fftn(l)
-	delta = - fft_l_eps[zero_] / fft_l[zero_]
-	w = xp.fft.ifftn((delta * fft_l + fft_l_eps) * sml_div)
-	ll = l * xp.fft.ifftn(xp.fft.fftn(l) * exp_alpha_nu.conj())
-	fft_wll = xp.fft.fftn(w / ll)
-	fft_ill = xp.fft.fftn(1.0 / ll)
-	w0 = - fft_wll[zero_] / fft_ill[zero_]
-	del_l = xp.fft.ifftn((fft_wll + w0 * fft_ill) * sml_div.conj())
-	h = xp.real(h_thresh + del_l * l)
-	lam = xp.real(lam + delta)
-	arg_v = phi + 2.0 * xp.pi * xp.tensordot(xp.asarray(alpha), h, axes=0)
-	err = xp.abs(xp.fft.ifftn(lk_alpha_nu * xp.fft.fftn(h)) + lam + dv(arg_v, eps)).max()
-	return h, lam, err
+    def __str__(self):
+        return 'Quasiperiodic Frenkel-Kontorova ({self.__class__.name__}) model with alpha = {self.alpha}'.format(self=self)
 
-def converge_point(eps1, eps2):
-	h = - xp.fft.ifftn(xp.fft.fftn(dv(phi, xp.array([eps1, eps2]))) * ilk_alpha_nu)
-	lam = 0.0
-	err = 1.0
-	it_count = 0
-	while TolMax >= err >= TolMin:
-		h, lam, err = refine_h(h, lam, xp.array([eps1, eps2]))
-		it_count += 1
-	if err <= TolMin:
-		it_count = 0
-	return [(err <= TolMin), it_count]
+	def refine_h(self, h, lam, eps):
+		fft_h = xp.fft.fftn(h)
+		fft_h[xp.abs(fft_h) <= self.threshold] = 0.0
+		h_thresh = xp.fft.ifftn(fft_h)
+		arg_v = self.phi + 2.0 * xp.pi * xp.tensordot(xp.asarray(self.alpha), h_thresh, axes=0)
+		l = 1.0 + xp.fft.ifftn(1j * self.alpha_nu * fft_h)
+		epsilon = xp.fft.ifftn(self.lk_alpha_nu * fft_h) + lam + self.dv(arg_v, eps)
+		fft_l_eps = xp.fft.fftn(l * epsilon)
+		fft_l = xp.fft.fftn(l)
+		delta = - fft_l_eps[self.zero_] / fft_l[self.zero_]
+		w = xp.fft.ifftn((delta * fft_l + fft_l_eps) * self.sml_div)
+		ll = l * xp.fft.ifftn(xp.fft.fftn(l) * self.exp_alpha_nu.conj())
+		fft_wll = xp.fft.fftn(w / ll)
+		fft_ill = xp.fft.fftn(1.0 / ll)
+		w0 = - fft_wll[self.zero_] / fft_ill[self.zero_]
+		del_l = xp.fft.ifftn((fft_wll + w0 * fft_ill) * self.sml_div.conj())
+		h = xp.real(h_thresh + del_l * l)
+		lam = xp.real(lam + delta)
+		arg_v = self.phi + 2.0 * xp.pi * xp.tensordot(xp.asarray(self.alpha), h, axes=0)
+		err = xp.abs(xp.fft.ifftn(self.lk_alpha_nu * xp.fft.fftn(h)) + lam + self.dv(arg_v, eps)).max()
+		return h, lam, err
 
-def save_data(name, data, timestr):
-	mdic = dict({'alpha': alpha, 'N': N, 'threshold': threshold / N**dim, 'TolMin': TolMin, 'TolMax': TolMax, 'epsilon': eps_vecs, 'potential': potential})
+def save_data(name, data, timestr, case):
+	mdic = case.DictParams
 	mdic.update({'data': data})
 	today = date.today()
 	date_today = today.strftime(" %B %d, %Y\n")
@@ -81,20 +102,17 @@ def save_data(name, data, timestr):
 	mdic.update({'date': date_today, 'author': email})
 	savemat(name + '_' + timestr + '.mat', mdic)
 
-def main():
-	timestr = time.strftime("%Y%m%d_%H%M")
-	num_cores = multiprocessing.cpu_count()
-	pool = multiprocessing.Pool(num_cores)
-	data = []
-	for eps2 in tqdm(eps_vecs[1]):
-		converge_point_ = partial(converge_point, eps2=eps2)
-		for result in pool.imap(converge_point_, iterable=eps_vecs[0]):
-			data.append(result)
-		save_data('qpFK_converge_region', data, timestr)
-	data = xp.array(data).reshape((n_eps, n_eps, 2))
-	save_data('qpFK_converge_region', data, timestr)
-	plt.pcolor(data[:, :, 0])
-	plt.show()
+def converge_point(eps1, eps2, case):
+	h = case.initial_h([eps1, eps2])
+	lam = 0.0
+	err = 1.0
+	it_count = 0
+	while case.TolMax >= err >= case.TolMin:
+		h, lam, err = case.refine_h(h, lam, [eps1, eps2])
+		it_count += 1
+	if err <= case.TolMin:
+		it_count = 0
+	return [(err <= case.TolMin), it_count]
 
 if __name__ == "__main__":
 	main()
