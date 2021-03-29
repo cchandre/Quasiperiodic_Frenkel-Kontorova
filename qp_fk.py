fcfa0a9fede020a09c0afea02cc53294a7788989
Cristel Chandre <69516423+cchandre@users.noreply.github.com>
2021-03-29 11:46:21 +0200

    Update qp_fk.py

--- qp_fk.py
+++ qp_fk.py
@@ -2,7 +2,7 @@ import numpy as xp
 from numpy.fft import fftn, ifftn, fftfreq
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-import multiprocessing
+import multiprocess
 from scipy.io import savemat
 import time
 from datetime import date
@@ -11,13 +11,13 @@ warnings.filterwarnings("ignore")
 
 def main():
 	dict_params = {
-		'N': 2 ** 8,
+		'N': 2 ** 9,
 		'alpha': [1.246979603717467, 2.801937735804838],
 		'potential': 'pot1',
-		'n_eps': 30,
+		'n_eps': 256,
 		'eps_region': [[0.007, 0.0025], [0.014,  0.0038]],
 		'eps_point': [0.009, 0.0030],
-		'eps_dir': [0.0, 0.02, xp.pi/5],
+		'eps_dir': [0.004, 0.006, xp.pi/5],
 		'TolMax': 1e5,
 		'TolMin': 1e-5,
 		'threshold': 1e-7,
@@ -36,10 +36,10 @@ def main():
 
 class qpFK:
 	def __init__(self, dv, dict_params):
-		for key in dict_param:
-            setattr(self, key, dict_params[key])
+		for key in dict_params:
+			setattr(self, key, dict_params[key])
 		self.DictParams = dict_params
-		self.Precision = {32: xp.float32, 64: xp.float64, 128: xp.float128}.get(self.Precision, xp.float64)()
+		self.Precision = {32: xp.float32, 64: xp.float64, 128: xp.float128}.get(self.Precision, xp.float64)
 		self.dv = dv
 		dim = len(self.alpha)
 		self.alpha = xp.asarray(self.alpha, dtype=self.Precision)
@@ -60,10 +60,10 @@ class qpFK:
 		self.initial_h = lambda eps: - ifftn(fftn(self.dv(self.phi, eps)) * ilk_alpha_nu)
 
 	def __repr__(self):
-        return '{self.__class__.name__}({self.dv, self.DictParams})'.format(self=self)
+		return '{self.__class__.name__}({self.dv, self.DictParams})'.format(self=self)
 
-    def __str__(self):
-        return 'Quasiperiodic Frenkel-Kontorova ({self.__class__.name__}) model with alpha = {self.alpha}'.format(self=self)
+	def __str__(self):
+		return 'Quasiperiodic Frenkel-Kontorova ({self.__class__.name__}) model with alpha = {self.alpha}'.format(self=self)
 
 	def refine_h(self, h, lam, eps):
 		fft_h = fftn(h)
@@ -88,8 +88,8 @@ class qpFK:
 		return h, lam, err
 
 	def norms(self, h, r):
-		return xp.sqrt(xp.abs(ifftn(self.alpha_nu ** r * fftn(h)) ** 2).sum()),\
-			xp.sqrt(xp.abs(ifftn(self.alpha_perp_nu ** r * fftn(h)) ** 2).sum())
+		return [xp.sqrt(xp.abs(ifftn(self.alpha_nu ** r * fftn(h)) ** 2).sum()),\
+			xp.sqrt(xp.abs(ifftn(self.alpha_perp_nu ** r * fftn(h)) ** 2).sum())]
 
 def save_data(name, data, timestr, case):
 	case.DictParams.update({'data': data})
@@ -108,30 +108,30 @@ def converge_point(eps1, eps2, case, gethull=False, getnorm=[False, 0]):
 	if gethull:
 		timestr = time.strftime("%Y%m%d_%H%M")
 		save_data('qpFK_hull', h, timestr, case)
-		return (err <= case.TolMin)
+		return int(err <= case.TolMin)
 	if getnorm[0]:
-		return (err <= case.TolMin), self.norm(h, getnorm[1])
+		return xp.append(int(err <= case.TolMin), case.norms(h, getnorm[1]))
 	if err <= case.TolMin:
 		it_count = 0
-	return [(err <= case.TolMin), it_count]
+	return [int(err <= case.TolMin), it_count]
 
 def converge_dir(r, case):
 	timestr = time.strftime("%Y%m%d_%H%M")
-	num_cores = multiprocessing.cpu_count()
-	pool = multiprocessing.Pool(num_cores)
+	num_cores = multiprocess.cpu_count()
+	pool = multiprocess.Pool(num_cores)
 	data = []
-	converge_dir_ = lambda eps1: converge_point(eps1, eps2=eps1 * xp.tan(case.eps_dir[2]), case=case, getnorm=[True, r])
-	for result in pool.imap(converge_dir_, iterable=xp.linspace(case.eps_dir[0], case.eps_dir[1], case.n_eps):
+	converge_dir_ = lambda eps1: converge_point(eps1, eps1 * xp.tan(case.eps_dir[2]), case, getnorm=[True, r])
+	for result in tqdm(pool.imap(converge_dir_, iterable=xp.linspace(case.eps_dir[0], case.eps_dir[1], case.n_eps))):
 		data.append(result)
-	save_data('qpFK_converge_dir', data, timestr, case)
+	save_data('qpFK_converge_dir', xp.array(data).reshape((case.n_eps, 3)), timestr, case)
 
 def converge_region(eps_region, case):
 	timestr = time.strftime("%Y%m%d_%H%M")
-	num_cores = multiprocessing.cpu_count()
-	pool = multiprocessing.Pool(num_cores)
+	num_cores = multiprocess.cpu_count()
+	pool = multiprocess.Pool(num_cores)
 	data = []
 	for eps2 in tqdm(eps_region[1]):
-		converge_point_ = lambda eps1: converge_point(eps1, eps2=eps2, case=case)
+		converge_point_ = lambda eps1: converge_point(eps1, eps2, case)
 		for result in pool.imap(converge_point_, iterable=eps_region[0]):
 			data.append(result)
 		save_data('qpFK_converge_region', data, timestr, case)
