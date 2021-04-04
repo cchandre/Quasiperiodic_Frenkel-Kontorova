8060052d90c41dde5c62012b79ed22728ea16836
Cristel Chandre <cchandre@gmail.com>
2021-04-04 13:57:41 +0200

    Minor edits
    - use of flake8

--- convergence.py
+++ convergence.py
@@ -5,84 +5,87 @@ from scipy.io import savemat
 import time
 from datetime import date
 
+
 def save_data(name, data, timestr, case, info=[]):
-	if case.save_results:
-		mdic = case.DictParams.copy()
-		mdic.update({'data': data, 'info': info})
-		date_today = date.today().strftime(" %B %d, %Y\n")
-		mdic.update({'date': date_today, 'author': 'cristel.chandre@univ-amu.fr'})
-		savemat(type(case).__name__ + '_' + name + '_' + timestr + '.mat', mdic)
+    if case.save_results:
+        mdic = case.DictParams.copy()
+        mdic.update({'data': data, 'info': info})
+        date_today = date.today().strftime(" %B %d, %Y\n")
+        mdic.update({'date': date_today, 'author': 'cristel.chandre@univ-amu.fr'})
+        savemat(type(case).__name__ + '_' + name + '_' + timestr + '.mat', mdic)
 
-def point(eps1, eps2, case, h = [], lam=[], gethull=False, getnorm=[False, 0]):
-	h_ = h.copy()
-	lam_ = lam.copy()
-	if len(h_) == 0:
-		h_, lam_ = case.initial_h([eps1, eps2])
-	err = 1.0
-	it_count = 0
-	while case.tolmax >= err >= case.tolmin:
-		h_, lam_, err = case.refine_h(h_, lam_, [eps1, eps2])
-		it_count += 1
-	if err <= case.tolmin:
-		it_count = 0
-	if gethull:
-		timestr = time.strftime("%Y%m%d_%H%M")
-		save_data('hull', h_, timestr, case)
-		return [int(err <= case.tolmin), it_count], h_, lam_
-	if getnorm[0]:
-		return [int(err <= case.tolmin), it_count], h_, lam_, case.norms(h_, getnorm[1])
-	return [int(err <= case.tolmin), it_count], h_, lam_
 
-def region(case, scale='lin', output= 'all', parallel=False):
-	timestr = time.strftime("%Y%m%d_%H%M")
-	if parallel:
-		num_cores = multiprocess.cpu_count()
-		pool = multiprocess.Pool(num_cores)
-	eps_region = xp.array(case.eps_region)
-	data = []
-	if case.eps_type == 'cartesian':
-		eps_grid = [xp.linspace(eps_region[0, 0], eps_region[0, 1], case.eps_n), xp.linspace(eps_region[1, 0], eps_region[1, 1], case.eps_n)]
-		for eps2 in tqdm(eps_grid[1]):
-			if parallel:
-				point_ = lambda eps1: point(eps1, eps2, case)
-				for result, h, lam in pool.imap(point_, iterable=eps_grid[0]):
-					data.append(result)
-			else:
-				h = []
-				lam = []
-				for eps1 in tqdm(eps_grid[0], leave=False):
-					result, h_, lam_ = point(eps1, eps2, case, h=h, lam=lam)
-					if result[0] == 1:
-						h = h_.copy()
-						lam = lam_.copy()
-					data.append(result)
-			save_data('region', data, timestr, case)
-	elif case.eps_type == 'polar':
-		thetas = xp.linspace(eps_region[1, 0], eps_region[1, 1], case.eps_n)
-		if output == 'all':
-			if scale == 'lin':
-				radii = xp.linspace(eps_region[0, 0], eps_region[0, 1], case.eps_n)
-			elif scale == 'log':
-				radii = eps_region[0, 1] * (1.0 - xp.logspace(xp.log10((eps_region[0, 1] - eps_region[0, 0]) / eps_region[0, 1]), xp.log10(case.tolmin), case.eps_n))
-			for theta in tqdm(thetas):
-				h = []
-				lam = []
-				for radius in tqdm(radii, leave=False):
-					result, h_, lam_ = point(radius * xp.cos(theta), radius * xp.sin(theta), case, h=h, lam=lam)
-					if result[0] == 1:
-						h = h_
-						lam = lam_
-					data.append(result)
-				save_data('region', data, timestr, case)
-		elif output == 'critical':
-			for theta in thetas:
-				eps_min, eps_max = eps_region[0]
-				while xp.abs(eps_min - eps_max) >= case.tolmin:
-					eps_mid = (eps_min + eps_max) / 2.0
-					if point(eps_mid * xp.cos(theta), eps_mid * xp.sin(theta), case)[0]:
-						eps_min = eps_mid
-					else:
-						eps_max = eps_mid
-				data.append(eps_min)
-	save_data('region', data, timestr, case)
-	return xp.array(data).reshape((case.eps_n, case.eps_n, -1))
+def point(eps1, eps2, case, h=[], lam=[], gethull=False, getnorm=[False, 0]):
+    h_ = h.copy()
+    lam_ = lam.copy()
+    if len(h_) == 0:
+        h_, lam_ = case.initial_h([eps1, eps2])
+    err = 1.0
+    it_count = 0
+    while case.tolmax >= err >= case.tolmin:
+        h_, lam_, err = case.refine_h(h_, lam_, [eps1, eps2])
+        it_count += 1
+    if err <= case.tolmin:
+        it_count = 0
+    if gethull:
+        timestr = time.strftime("%Y%m%d_%H%M")
+        save_data('hull', h_, timestr, case)
+        return [int(err <= case.tolmin), it_count], h_, lam_
+    if getnorm[0]:
+        return [int(err <= case.tolmin), it_count], h_, lam_, case.norms(h_, getnorm[1])
+    return [int(err <= case.tolmin), it_count], h_, lam_
+
+
+def region(case, scale='lin', output='all', parallel=False):
+    timestr = time.strftime("%Y%m%d_%H%M")
+    if parallel:
+        num_cores = multiprocess.cpu_count()
+        pool = multiprocess.Pool(num_cores)
+    eps_region = xp.array(case.eps_region)
+    data = []
+    if case.eps_type == 'cartesian':
+        eps_grid = [xp.linspace(eps_region[0, 0], eps_region[0, 1], case.eps_n), xp.linspace(eps_region[1, 0], eps_region[1, 1], case.eps_n)]
+        for eps2 in tqdm(eps_grid[1]):
+            if parallel:
+                point_ = lambda eps1: point(eps1, eps2, case)
+                for result, h, lam in pool.imap(point_, iterable=eps_grid[0]):
+                    data.append(result)
+            else:
+                h = []
+                lam = []
+                for eps1 in tqdm(eps_grid[0], leave=False):
+                    result, h_, lam_ = point(eps1, eps2, case, h=h, lam=lam)
+                    if result[0] == 1:
+                        h = h_.copy()
+                        lam = lam_.copy()
+                    data.append(result)
+            save_data('region', data, timestr, case)
+    elif case.eps_type == 'polar':
+        thetas = xp.linspace(eps_region[1, 0], eps_region[1, 1], case.eps_n)
+        if output == 'all':
+            if scale == 'lin':
+                radii = xp.linspace(eps_region[0, 0], eps_region[0, 1], case.eps_n)
+            elif scale == 'log':
+                radii = eps_region[0, 1] * (1.0 - xp.logspace(xp.log10((eps_region[0, 1] - eps_region[0, 0]) / eps_region[0, 1]), xp.log10(case.tolmin), case.eps_n))
+                for theta in tqdm(thetas):
+                    h = []
+                    lam = []
+                    for radius in tqdm(radii, leave=False):
+                        result, h_, lam_ = point(radius * xp.cos(theta), radius * xp.sin(theta), case, h=h, lam=lam)
+                        if result[0] == 1:
+                            h = h_
+                            lam = lam_
+                        data.append(result)
+                    save_data('region', data, timestr, case)
+        elif output == 'critical':
+            for theta in thetas:
+                eps_min, eps_max = eps_region[0]
+                while xp.abs(eps_min - eps_max) >= case.tolmin:
+                    eps_mid = (eps_min + eps_max) / 2.0
+                    if point(eps_mid * xp.cos(theta), eps_mid * xp.sin(theta), case)[0]:
+                        eps_min = eps_mid
+                    else:
+                        eps_max = eps_mid
+                data.append(eps_min)
+    save_data('region', data, timestr, case)
+    return xp.array(data).reshape((case.eps_n, case.eps_n, -1))
--- qp_fk.py
+++ qp_fk.py
@@ -5,106 +5,107 @@ import warnings
 import convergence as cv
 warnings.filterwarnings("ignore")
 
+
 def main():
-	# dict_params = {
-	# 	'n': 2 ** 9,
-	# 	'omega': 1.0,
-	# 	'alpha': [1.246979603717467, 2.801937735804838],
-	# 	'alpha_perp': [2.801937735804838, -1.246979603717467],
-	# 	'potential': 'pot1_2d'}
-	# dict_params.update({
-	# 	'eps_n': 32,
-	# 	'eps_region': [[0.0, 0.05], [0.0,  0.001]],
-	# 	'eps_point': [0.009, 0.0030],
-	# 	'eps_type': 'cartesian'})
-	dict_params = {
-		'n': 2 ** 10,
-		'omega': 0.618033988749895,
-		'alpha': [1.0],
-		'potential': 'pot1_1d'}
-	dict_params.update({
-		'eps_n': 256,
-		'eps_region': [[0.0, 2.0], [-0.8, 0.8]],
-		'eps_type': 'cartesian'})
-	dict_params.update({
-		'tolmax': 1e5,
-		'tolmin': 1e-10,
-		'threshold': 1e-12,
-		'precision': 64,
-		'save_results': False})
-	alpha = dict_params['alpha']
-	dv = {
-		'pot1_1d': lambda phi, eps: - alpha[0] / (2.0 * xp.pi) * (eps[0] * xp.sin(phi[0]) + eps[1] / 2.0 * xp.sin(2.0 * phi[0])),
-		'pot1_2d': lambda phi, eps: alpha[0] * eps[0] * xp.sin(phi[0]) + alpha[1] * eps[1] * xp.sin(phi[1]),
-		'pot2_2d': lambda phi, eps: alpha[0] * (eps[0] * xp.sin(2.0 * phi[0]+ 2.0 * phi[1]) + eps[1] * xp.sin(phi[0]))\
-		 	+ alpha[1] * (eps[0] * xp.sin(2.0 * phi[0]+ 2.0 * phi[1]) + eps[1] * xp.sin(phi[1]))
-		}.get(dict_params['potential'], 'pot1_2d')
-	case = qpFK(dv, dict_params)
-	data = cv.region(case, parallel=True)
-	plt.pcolor(data[:, :, 0])
-	plt.show()
-	#cv.point(case.eps_point[0], case.eps_point[1], case, gethull=True)
+    # dict_params = {
+    # 	'n': 2 ** 9,
+    # 	'omega': 1.0,
+    # 	'alpha': [1.246979603717467, 2.801937735804838],
+    # 	'alpha_perp': [2.801937735804838, -1.246979603717467],
+    # 	'potential': 'pot1_2d'}
+    # dict_params.update({
+    # 	'eps_n': 32,
+    # 	'eps_region': [[0.0, 0.05], [0.0,  0.001]],
+    # 	'eps_point': [0.009, 0.0030],
+    # 	'eps_type': 'cartesian'})
+    dict_params = {
+        'n': 2 ** 10,
+        'omega': 0.618033988749895,
+        'alpha': [1.0],
+        'potential': 'pot1_1d'}
+    dict_params.update({
+        'eps_n': 256,
+        'eps_region': [[0.0, 2.0], [-0.8, 0.8]],
+        'eps_type': 'cartesian'})
+    dict_params.update({
+        'tolmax': 1e5,
+        'tolmin': 1e-10,
+        'threshold': 1e-12,
+        'precision': 64,
+        'save_results': False})
+    alpha = dict_params['alpha']
+    dv = {
+        'pot1_1d': lambda phi, eps: - alpha[0] / (2.0 * xp.pi) * (eps[0] * xp.sin(phi[0]) + eps[1] / 2.0 * xp.sin(2.0 * phi[0])),
+        'pot1_2d': lambda phi, eps: alpha[0] * eps[0] * xp.sin(phi[0]) + alpha[1] * eps[1] * xp.sin(phi[1]),
+        'pot2_2d': lambda phi, eps: alpha[0] * (eps[0] * xp.sin(2.0 * phi[0] + 2.0 * phi[1]) + eps[1] * xp.sin(phi[0])) + alpha[1] * (eps[0] * xp.sin(2.0 * phi[0] + 2.0 * phi[1]) + eps[1] * xp.sin(phi[1]))
+    }.get(dict_params['potential'], 'pot1_2d')
+    case = qpFK(dv, dict_params)
+    data = cv.region(case, parallel=True)
+    plt.pcolor(data[:, :, 0])
+    plt.show()
+    # cv.point(case.eps_point[0], case.eps_point[1], case, gethull=True)
+
 
 class qpFK:
-	def __repr__(self):
-		return '{self.__class__.name__}({self.dv, self.DictParams})'.format(self=self)
+    def __repr__(self):
+        return '{self.__class__.name__}({self.dv, self.DictParams})'.format(self=self)
 
-	def __str__(self):
-		return 'Quasiperiodic Frenkel-Kontorova ({self.__class__.name__}) model with alpha = {self.alpha} and omega = {self.omega}'.format(self=self)
+    def __str__(self):
+        return 'Quasiperiodic Frenkel-Kontorova ({self.__class__.name__}) model with alpha = {self.alpha} and omega = {self.omega}'.format(self=self)
 
-	def __init__(self, dv, dict_params):
-		for key in dict_params:
-			setattr(self, key, dict_params[key])
-		self.DictParams = dict_params
-		self.precision = {32: xp.float32, 64: xp.float64, 128: xp.float128}.get(self.precision, xp.float64)
-		self.dv = dv
-		self.dim = len(self.alpha)
-		self.alpha = xp.array(self.alpha, self.precision)
-		self.zero_ = self.dim * (0,)
-		ind_nu = self.dim * (fftfreq(self.n, d=1.0/self.precision(self.n)),)
-		nu = xp.asarray(xp.meshgrid(*ind_nu, indexing='ij'), dtype=self.precision)
-		self.alpha_nu = xp.einsum('i,i...->...', self.alpha, nu)
-		if hasattr(self, 'alpha_perp'):
-			self.alpha_perp = xp.array(self.alpha_perp, self.precision)
-			self.alpha_perp_nu = xp.einsum('i,i...->...', self.alpha_perp, nu)
-		self.exp_alpha_nu = xp.exp(1j * 2.0 * xp.pi * self.omega * self.alpha_nu)
-		self.lk_alpha_nu = 2.0 * (xp.cos(2.0 * xp.pi * self.omega * self.alpha_nu) - 1.0)
-		self.sml_div = self.exp_alpha_nu - 1.0
-		self.sml_div = xp.divide(1.0, self.sml_div, where=self.sml_div!=0)
-		ind_phi = self.dim * (xp.linspace(0.0, 2.0 * xp.pi, self.n, endpoint=False),)
-		self.phi = xp.asarray(xp.meshgrid(*ind_phi, indexing='ij'), dtype=self.precision)
-		self.threshold *= self.n**self.dim
-		self.ilk_alpha_nu = xp.divide(1.0, self.lk_alpha_nu, where=self.lk_alpha_nu!=0)
-		self.initial_h = lambda eps: [- ifftn(fftn(self.dv(self.phi, eps)) * self.ilk_alpha_nu), 0.0]
+    def __init__(self, dv, dict_params):
+        for key in dict_params:
+            setattr(self, key, dict_params[key])
+        self.DictParams = dict_params
+        self.precision = {32: xp.float32, 64: xp.float64, 128: xp.float128}.get(self.precision, xp.float64)
+        self.dv = dv
+        self.dim = len(self.alpha)
+        self.alpha = xp.array(self.alpha, self.precision)
+        self.zero_ = self.dim * (0,)
+        ind_nu = self.dim * (fftfreq(self.n, d=1.0/self.precision(self.n)),)
+        nu = xp.asarray(xp.meshgrid(*ind_nu, indexing='ij'), dtype=self.precision)
+        self.alpha_nu = xp.einsum('i,i...->...', self.alpha, nu)
+        if hasattr(self, 'alpha_perp'):
+            self.alpha_perp = xp.array(self.alpha_perp, self.precision)
+            self.alpha_perp_nu = xp.einsum('i,i...->...', self.alpha_perp, nu)
+        self.exp_alpha_nu = xp.exp(1j * 2.0 * xp.pi * self.omega * self.alpha_nu)
+        self.lk_alpha_nu = 2.0 * (xp.cos(2.0 * xp.pi * self.omega * self.alpha_nu) - 1.0)
+        self.sml_div = self.exp_alpha_nu - 1.0
+        self.sml_div = xp.divide(1.0, self.sml_div, where=self.sml_div != 0)
+        ind_phi = self.dim * (xp.linspace(0.0, 2.0 * xp.pi, self.n, endpoint=False),)
+        self.phi = xp.asarray(xp.meshgrid(*ind_phi, indexing='ij'), dtype=self.precision)
+        self.threshold *= self.n ** self.dim
+        self.ilk_alpha_nu = xp.divide(1.0, self.lk_alpha_nu, where=self.lk_alpha_nu != 0)
+        self.initial_h = lambda eps: [- ifftn(fftn(self.dv(self.phi, eps)) * self.ilk_alpha_nu), 0.0]
 
-	def refine_h(self, h, lam, eps):
-		fft_h = fftn(h)
-		fft_h[xp.abs(fft_h) <= self.threshold] = 0.0
-		h_thresh = ifftn(fft_h)
-		arg_v = self.phi + 2.0 * xp.pi * xp.tensordot(self.alpha, h_thresh, axes=0)
-		l = 1.0 + 2.0 * xp.pi * ifftn(1j * self.alpha_nu * fft_h)
-		epsilon = ifftn(self.lk_alpha_nu * fft_h) + lam + self.dv(arg_v, eps)
-		fft_leps = fftn(l * epsilon)
-		fft_l = fftn(l)
-		delta = - fft_leps[self.zero_] / fft_l[self.zero_]
-		w = ifftn((delta * fft_l + fft_leps) * self.sml_div)
-		ll = l * ifftn(xp.fft.fftn(l) * self.exp_alpha_nu.conj())
-		fft_wll = fftn(w / ll)
-		fft_ill = fftn(1.0 / ll)
-		w0 = - fft_wll[self.zero_] / fft_ill[self.zero_]
-		dell = ifftn((fft_wll + w0 * fft_ill) * self.sml_div.conj())
-		h = xp.real(h_thresh + dell * l)
-		lam = xp.real(lam + delta)
-		arg_v = self.phi + 2.0 * xp.pi * xp.tensordot(self.alpha, h, axes=0)
-		err = xp.abs(ifftn(self.lk_alpha_nu * fftn(h)) + lam + self.dv(arg_v, eps)).max()
-		return h, lam, err
+    def refine_h(self, h, lam, eps):
+        fft_h = fftn(h)
+        fft_h[xp.abs(fft_h) <= self.threshold] = 0.0
+        h_thresh = ifftn(fft_h)
+        arg_v = self.phi + 2.0 * xp.pi * xp.tensordot(self.alpha, h_thresh, axes=0)
+        lfunc = 1.0 + 2.0 * xp.pi * ifftn(1j * self.alpha_nu * fft_h)
+        epsilon = ifftn(self.lk_alpha_nu * fft_h) + lam + self.dv(arg_v, eps)
+        fft_leps = fftn(lfunc * epsilon)
+        fft_l = fftn(lfunc)
+        delta = - fft_leps[self.zero_] / fft_l[self.zero_]
+        w = ifftn((delta * fft_l + fft_leps) * self.sml_div)
+        ll = lfunc * ifftn(fft_l * self.exp_alpha_nu.conj())
+        fft_wll = fftn(w / ll)
+        fft_ill = fftn(1.0 / ll)
+        w0 = - fft_wll[self.zero_] / fft_ill[self.zero_]
+        dell = ifftn((fft_wll + w0 * fft_ill) * self.sml_div.conj())
+        h = xp.real(h_thresh + dell * lfunc)
+        lam = xp.real(lam + delta)
+        arg_v = self.phi + 2.0 * xp.pi * xp.tensordot(self.alpha, h, axes=0)
+        err = xp.abs(ifftn(self.lk_alpha_nu * fftn(h)) + lam + self.dv(arg_v, eps)).max()
+        return h, lam, err
 
-	def norms(self, h, r=0):
-		if hasattr(self, 'alpha_perp'):
-			return [xp.sqrt(xp.abs(ifftn(self.alpha_nu ** r * fftn(h)) ** 2).sum()),\
-				xp.sqrt(xp.abs(ifftn(self.alpha_perp_nu ** r * fftn(h)) ** 2).sum())]
-		else:
-			return xp.sqrt(xp.abs(ifftn(self.alpha_nu ** r * fftn(h)) ** 2).sum())
+    def norms(self, h, r=0):
+        if hasattr(self, 'alpha_perp'):
+            return [xp.sqrt(xp.abs(ifftn(self.alpha_nu ** r * fftn(h)) ** 2).sum()), xp.sqrt(xp.abs(ifftn(self.alpha_perp_nu ** r * fftn(h)) ** 2).sum())]
+        else:
+            return xp.sqrt(xp.abs(ifftn(self.alpha_nu ** r * fftn(h)) ** 2).sum())
+
 
 if __name__ == "__main__":
-	main()
+    main()
