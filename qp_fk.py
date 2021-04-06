84f2dd6d03573cdec5c5c93dfc28aa459c351169
Cristel Chandre <cchandre@gmail.com>
2021-04-06 17:48:08 +0200

    Minor corrections
    - correction of minor bugs; still to be debugged...

--- qp_fk.py
+++ qp_fk.py
@@ -7,30 +7,29 @@ warnings.filterwarnings("ignore")
 
 
 def main():
-    # dict_params = {
-    # 	'n': 2 ** 9,
-    # 	'omega': 1.0,
-    # 	'alpha': [1.246979603717467, 2.801937735804838],
-    # 	'alpha_perp': [2.801937735804838, -1.246979603717467],
-    # 	'potential': 'pot1_2d'}
-    # dict_params.update({
-    # 	'eps_n': 32,
-    # 	'eps_region': [[0.0, 0.05], [0.0,  0.001]],
-    # 	'eps_point': [0.009, 0.0030],
-    # 	'eps_type': 'cartesian'})
     dict_params = {
-        'n': 2 ** 10,
-        'omega': 0.618033988749895,
-        'alpha': [1.0],
-        'potential': 'pot1_1d'}
+    	'n': 2 ** 9,
+    	'omega': 1.0,
+    	'alpha': [1.246979603717467, 2.801937735804838],
+    	'alpha_perp': [2.801937735804838, -1.246979603717467],
+    	'potential': 'pot1_2d'}
     dict_params.update({
-        'eps_n': 256,
-        'eps_region': [[0.0, 2.0], [-0.8, 0.8]],
-        'eps_type': 'cartesian'})
+    	'eps_n': 30,
+    	'eps_region': [[0.0, 0.05], [0.0,  0.001]],
+    	'eps_type': 'cartesian'})
+    # dict_params = {
+    #     'n': 2 ** 10,
+    #     'omega': 0.618033988749895,
+    #     'alpha': [1.0],
+    #     'potential': 'pot1_1d'}
+    # dict_params.update({
+    #     'eps_n': 128,
+    #     'eps_region': [[0.0, 2.0], [-0.8, 0.8]],
+    #     'eps_type': 'cartesian'})
     dict_params.update({
         'tolmax': 1e5,
         'tolmin': 1e-10,
-        'threshold': 1e-12,
+        'threshold': 1e-14,
         'precision': 64,
         'save_results': False})
     alpha = dict_params['alpha']
@@ -40,7 +39,7 @@ def main():
         'pot2_2d': lambda phi, eps: alpha[0] * (eps[0] * xp.sin(2.0 * phi[0] + 2.0 * phi[1]) + eps[1] * xp.sin(phi[0])) + alpha[1] * (eps[0] * xp.sin(2.0 * phi[0] + 2.0 * phi[1]) + eps[1] * xp.sin(phi[1]))
     }.get(dict_params['potential'], 'pot1_2d')
     case = qpFK(dv, dict_params)
-    data = cv.region(case, parallel=True)
+    data = cv.region(case, parallel=False)
     plt.pcolor(data[:, :, 0])
     plt.show()
 
@@ -70,12 +69,12 @@ class qpFK:
         self.exp_alpha_nu = xp.exp(2j * xp.pi * self.omega * self.alpha_nu)
         self.lk_alpha_nu = 2.0 * (xp.cos(2.0 * xp.pi * self.omega * self.alpha_nu) - 1.0)
         self.sml_div = self.exp_alpha_nu - 1.0
-        self.sml_div = xp.divide(1.0, self.sml_div, where=self.sml_div != 0)
+        self.sml_div = xp.divide(1.0, self.sml_div, where=self.sml_div!=0)
         ind_phi = dim * (xp.linspace(0.0, 2.0 * xp.pi, self.n, endpoint=False),)
         self.phi = xp.asarray(xp.meshgrid(*ind_phi, indexing='ij'), dtype=self.precision)
         self.rescale_fft = self.n ** dim
         self.threshold *= self.rescale_fft
-        self.ilk_alpha_nu = xp.divide(1.0, self.lk_alpha_nu, where=self.lk_alpha_nu != 0)
+        self.ilk_alpha_nu = xp.divide(1.0, self.lk_alpha_nu, where=self.lk_alpha_nu!=0)
         self.initial_h = lambda eps: [- ifftn(fftn(self.dv(self.phi, eps)) * self.ilk_alpha_nu), 0.0]
 
     def refine_h(self, h, lam, eps):
@@ -86,8 +85,8 @@ class qpFK:
         lfunc = 1.0 + 2j * xp.pi * ifftn(self.alpha_nu * fft_h)
         epsilon = ifftn(self.lk_alpha_nu * fft_h) + lam + self.dv(arg_v, eps)
         fft_leps = fftn(lfunc * epsilon)
-        fft_l = 2j * xp.pi * self.alpha_nu * fft_h / self.rescale_fft
-        fft_l[self_zero_] = 1.0 / self.rescale_fft
+        fft_l = 2j * xp.pi * self.alpha_nu * fft_h * self.rescale_fft
+        fft_l[self.zero_] = 1.0 * self.rescale_fft
         delta = - fft_leps[self.zero_] / fft_l[self.zero_]
         w = ifftn((delta * fft_l + fft_leps) * self.sml_div)
         ll = lfunc * ifftn(fft_l * self.exp_alpha_nu.conj())
