5f7400fb6304d116ab8ddb3234eecb6f56a334c2
Cristel Chandre <cchandre@gmail.com>
2021-09-08 12:58:44 +0200

    Update qpfk.py

--- qpfk.py
+++ qpfk.py
@@ -91,6 +91,8 @@ class qpFK:
         h_ = ifftn(fft_h).real
         arg_v = (self.phi + xp.tensordot(self.Omega, h_, axes=0)) % (2.0 * xp.pi)
         err = xp.abs(ifftn(self.lk * fft_h).real + self.Dv(arg_v, eps, self.Omega) + lam_).max()
+        if self.AdaptSize and (tail_norm >= self.Threshold) and (h.shape[0] >= self.Lmax):
+			err = self.TolMax ** 2
         if self.MonitorGrad:
             dh_ = self.id + xp.tensordot(self.Omega, xp.gradient(h_, 2.0 * xp.pi / self.Precision(h_.shape[0])), axes=0)
             det_h_ = xp.abs(LA.det(xp.moveaxis(dh_, [0, 1], [-2, -1]))).min()
